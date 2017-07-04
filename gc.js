3d791c713c4059ac26c8143021cd33add3501553
be5invis <belleve@typeof.net>
2017-07-04 22:42:53 +0800

    gc : add better subtable handler. currently gsub_single.

--- gc.js
+++ gc.js
@@ -20,17 +20,44 @@ function mark(lut, obj) {
 	}
 }
 
+function markSubtable(lut, type, st) {
+	switch (type) {
+		case "gsub_single":
+		case "gsub_multi":
+		case "gsub_alternate":
+			for (let k in st) if (lut[k]) mark(lut, st[k]);
+			break;
+		default:
+			mark(lut, st);
+			break;
+	}
+}
+
 module.exports = async function(ctx, target) {
 	const font = this.items[target];
 	for (let passes = 0; passes < 16; passes++) {
 		let lut = {};
-		mark(lut, font.cmap);
-		mark(lut, font.cmap_uvs);
-		mark(lut, font.GSUB);
-		// mark(lut, font.GPOS);
-		if (font.glyph_order) {
-			mark(lut, font.glyph_order[0]);
-		}
+		let lutn = 0;
+		do {
+			lutn = Object.keys(lut).length;
+			if (font.glyph_order) {
+				mark(lut, font.glyph_order[0]);
+			}
+			mark(lut, font.cmap);
+			mark(lut, font.cmap_uvs);
+			if (font.GSUB) {
+				for (let l in font.GSUB.lookups) {
+					const lookup = font.GSUB.lookups[l];
+					if (lookup && lookup.subtables) {
+						for (let st of lookup.subtables) {
+							markSubtable(lut, lookup.type, st);
+						}
+					}
+				}
+			}
+			let lutn1 = Object.keys(lut).length;
+			if (lutn1 === lutn) break;
+		} while (true);
 
 		let na = 0;
 		let nk = 0;

8f4c3dfe5f21230c010f254738f130f82ebf903f
be5invis <belleve@typeof.net>
2017-12-17 15:23:33 +0800

    "modernize" ctqcore and fix an error about estimating V parameter

--- procs/quadify/ctqcore.js
+++ procs/quadify/ctqcore.js
@@ -66,13 +66,13 @@ function calcPowerCoefficients(p1, c1, c2, p2) {
 	// b = 3 * (p1 + c2) - 6 * c1
 	// c = 3 * (c1 - p1)
 	// d = p1
-	var a = p2.sub(p1).add(c1.sub(c2).mul(3));
-	var b = p1
+	let a = p2.sub(p1).add(c1.sub(c2).mul(3));
+	let b = p1
 		.add(c2)
 		.mul(3)
 		.sub(c1.mul(6));
-	var c = c1.sub(p1).mul(3);
-	var d = p1;
+	let c = c1.sub(p1).mul(3);
+	let d = p1;
 	return [a, b, c, d];
 }
 
@@ -110,13 +110,13 @@ function quadSolve(a, b, c) {
 	if (a === 0) {
 		return b === 0 ? [] : [-c / b];
 	}
-	var D = b * b - 4 * a * c;
+	let D = b * b - 4 * a * c;
 	if (D < 0) {
 		return [];
 	} else if (D === 0) {
 		return [-b / (2 * a)];
 	}
-	var DSqrt = Math.sqrt(D);
+	let DSqrt = Math.sqrt(D);
 	return [(-b - DSqrt) / (2 * a), (-b + DSqrt) / (2 * a)];
 }
 
@@ -131,23 +131,23 @@ function cubicSolve(a, b, c, d) {
 	}
 	// solve using Cardan's method, which is described in paper of R.W.D. Nickals
 	// http://www.nickalls.org/dick/papers/maths/cubic1993.pdf (doi:10.2307/3619777)
-	var xn = -b / (3 * a); // point of symmetry x coordinate
-	var yn = ((a * xn + b) * xn + c) * xn + d; // point of symmetry y coordinate
-	var deltaSq = (b * b - 3 * a * c) / (9 * a * a); // delta^2
-	var hSq = 4 * a * a * Math.pow(deltaSq, 3); // h^2
-	var D3 = yn * yn - hSq;
+	let xn = -b / (3 * a); // point of symmetry x coordinate
+	let yn = ((a * xn + b) * xn + c) * xn + d; // point of symmetry y coordinate
+	let deltaSq = (b * b - 3 * a * c) / (9 * a * a); // delta^2
+	let hSq = 4 * a * a * Math.pow(deltaSq, 3); // h^2
+	let D3 = yn * yn - hSq;
 	if (D3 > 0) {
 		// 1 real root
-		var D3Sqrt = Math.sqrt(D3);
+		let D3Sqrt = Math.sqrt(D3);
 		return [xn + cubicRoot((-yn + D3Sqrt) / (2 * a)) + cubicRoot((-yn - D3Sqrt) / (2 * a))];
 	} else if (D3 === 0) {
 		// 2 real roots
-		var delta1 = cubicRoot(yn / (2 * a));
+		let delta1 = cubicRoot(yn / (2 * a));
 		return [xn - 2 * delta1, xn + delta1];
 	}
 	// 3 real roots
-	var theta = Math.acos(-yn / Math.sqrt(hSq)) / 3;
-	var delta = Math.sqrt(deltaSq);
+	let theta = Math.acos(-yn / Math.sqrt(hSq)) / 3;
+	let delta = Math.sqrt(deltaSq);
 	return [
 		xn + 2 * delta * Math.cos(theta),
 		xn + 2 * delta * Math.cos(theta + Math.PI * 2 / 3),
@@ -174,22 +174,22 @@ function minDistanceToQuad(point, p1, c1, p2) {
 	// So to find the minimal distance one have to just pick the minimum value of
 	// the distance on set {t = 0 | t = 1 | t is root of the equation from [0, 1] }.
 
-	var a = p1.add(p2).sub(c1.mul(2));
-	var b = c1.sub(p1).mul(2);
-	var c = p1;
-	var e3 = 2 * a.sqr();
-	var e2 = 3 * a.dot(b);
-	var e1 = b.sqr() + 2 * a.dot(c.sub(point));
-	var e0 = c.sub(point).dot(b);
-	var candidates = cubicSolve(e3, e2, e1, e0)
+	let a = p1.add(p2).sub(c1.mul(2));
+	let b = c1.sub(p1).mul(2);
+	let c = p1;
+	let e3 = 2 * a.sqr();
+	let e2 = 3 * a.dot(b);
+	let e1 = b.sqr() + 2 * a.dot(c.sub(point));
+	let e0 = c.sub(point).dot(b);
+	let candidates = cubicSolve(e3, e2, e1, e0)
 		.filter(function(t) {
 			return t > 0 && t < 1;
 		})
 		.concat([0, 1]);
 
-	var minDistance = 1e9;
-	for (var i = 0; i < candidates.length; i++) {
-		var distance = calcPointQuad(a, b, c, candidates[i])
+	let minDistance = 1e9;
+	for (let i = 0; i < candidates.length; i++) {
+		let distance = calcPointQuad(a, b, c, candidates[i])
 			.sub(point)
 			.dist();
 		if (distance < minDistance) {
@@ -226,8 +226,10 @@ function processSegment(a, b, c, d, t1, t2, check) {
 	const D = ad.x * bd.y - ad.y * bd.x;
 	if (check) {
 		if (Math.abs(D) < 1e-8) return null;
-		const u = (as.y * bd.x + bd.y * bs.x - bs.y * bd.x - bd.y * as.x) / D;
-		const v = (as.x + ad.x * u - bs.x) / bd.x;
+		const dx = bs.x - as.x;
+		const dy = bs.y - as.y;
+		const u = -(dy * bd.x - dx * bd.y) / D;
+		const v = -(dy * ad.x - dx * ad.y) / D;
 		if (u > 0 && v < 0) {
 			return [as, as.add(ad.mul(u)), bs];
 		} else {
@@ -239,6 +241,8 @@ function processSegment(a, b, c, d, t1, t2, check) {
 		return [as, as.add(ad.mul(u)), bs];
 	}
 }
+
+const SAMPLES = 16;
 
 function isSegmentApproximationClose(a, b, c, d, tmin, tmax, p1, c1, p2, errorBound) {
 	// a,b,c,d define cubic curve
@@ -257,12 +261,11 @@ function isSegmentApproximationClose(a, b, c, d, tmin, tmax, p1, c1, p2, errorBo
 	// But this method allows easy estimation of approximation error, so it is enough
 	// for practical purposes.
 
-	var n = 10; // number of points + 1
-	var dt = (tmax - tmin) / n;
-	for (var t = tmin + dt; t < tmax - dt; t += dt) {
+	const dt = (tmax - tmin) / SAMPLES;
+	for (let t = tmin + dt; t < tmax - dt; t += dt) {
 		// don't check distance on boundary points
 		// because they should be the same
-		var point = calcPoint(a, b, c, d, t);
+		const point = calcPoint(a, b, c, d, t);
 		if (minDistanceToQuad(point, p1, c1, p2) > errorBound) {
 			return false;
 		}
@@ -271,11 +274,11 @@ function isSegmentApproximationClose(a, b, c, d, tmin, tmax, p1, c1, p2, errorBo
 }
 
 function _isApproximationClose(a, b, c, d, quadCurves, errorBound) {
-	var dt = 1 / quadCurves.length;
-	for (var i = 0; i < quadCurves.length; i++) {
-		var p1 = quadCurves[i][0];
-		var c1 = quadCurves[i][1];
-		var p2 = quadCurves[i][2];
+	let dt = 1 / quadCurves.length;
+	for (let i = 0; i < quadCurves.length; i++) {
+		let p1 = quadCurves[i][0];
+		let c1 = quadCurves[i][1];
+		let p2 = quadCurves[i][2];
 		if (
 			!isSegmentApproximationClose(a, b, c, d, i * dt, (i + 1) * dt, p1, c1, p2, errorBound)
 		) {
@@ -286,9 +289,9 @@ function _isApproximationClose(a, b, c, d, quadCurves, errorBound) {
 }
 
 function fromFlatArray(points) {
-	var result = [];
-	var segmentsNumber = (points.length - 2) / 4;
-	for (var i = 0; i < segmentsNumber; i++) {
+	let result = [];
+	let segmentsNumber = (points.length - 2) / 4;
+	for (let i = 0; i < segmentsNumber; i++) {
 		result.push([
 			new Point(points[4 * i], points[4 * i + 1]),
 			new Point(points[4 * i + 2], points[4 * i + 3]),
@@ -299,10 +302,10 @@ function fromFlatArray(points) {
 }
 
 function toFlatArray(quadsList) {
-	var result = [];
+	let result = [];
 	result.push(quadsList[0][0].x);
 	result.push(quadsList[0][0].y);
-	for (var i = 0; i < quadsList.length; i++) {
+	for (let i = 0; i < quadsList.length; i++) {
 		result.push(quadsList[i][1].x);
 		result.push(quadsList[i][1].y);
 		result.push(quadsList[i][2].x);
@@ -313,7 +316,7 @@ function toFlatArray(quadsList) {
 
 function isApproximationClose(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y, quads, errorBound) {
 	// TODO: rewrite it in C-style and remove _isApproximationClose
-	var pc = calcPowerCoefficients(
+	let pc = calcPowerCoefficients(
 		new Point(p1x, p1y),
 		new Point(c1x, c1y),
 		new Point(c2x, c2y),
@@ -327,22 +330,22 @@ function isApproximationClose(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y, quads, err
  * https://math.stackexchange.com/questions/877725
  */
 function subdivideCubic(x1, y1, x2, y2, x3, y3, x4, y4, t) {
-	var u = 1 - t,
+	let u = 1 - t,
 		v = t;
 
-	var bx = x1 * u + x2 * v;
-	var sx = x2 * u + x3 * v;
-	var fx = x3 * u + x4 * v;
-	var cx = bx * u + sx * v;
-	var ex = sx * u + fx * v;
-	var dx = cx * u + ex * v;
+	let bx = x1 * u + x2 * v;
+	let sx = x2 * u + x3 * v;
+	let fx = x3 * u + x4 * v;
+	let cx = bx * u + sx * v;
+	let ex = sx * u + fx * v;
+	let dx = cx * u + ex * v;
 
-	var by = y1 * u + y2 * v;
-	var sy = y2 * u + y3 * v;
-	var fy = y3 * u + y4 * v;
-	var cy = by * u + sy * v;
-	var ey = sy * u + fy * v;
-	var dy = cy * u + ey * v;
+	let by = y1 * u + y2 * v;
+	let sy = y2 * u + y3 * v;
+	let fy = y3 * u + y4 * v;
+	let cy = by * u + sy * v;
+	let ey = sy * u + fy * v;
+	let dy = cy * u + ey * v;
 
 	return [[x1, y1, bx, by, cx, cy, dx, dy], [dx, dy, ex, ey, fx, fy, x4, y4]];
 }
@@ -356,17 +359,17 @@ function byNumber(x, y) {
  * http://www.caffeineowl.com/graphics/2d/vectorial/cubic-inflexion.html
  */
 function solveInflections(x1, y1, x2, y2, x3, y3, x4, y4) {
-	var p =
+	let p =
 		-(x4 * (y1 - 2 * y2 + y3)) +
 		x3 * (2 * y1 - 3 * y2 + y4) +
 		x1 * (y2 - 2 * y3 + y4) -
 		x2 * (y1 - 3 * y3 + 2 * y4);
-	var q =
+	let q =
 		x4 * (y1 - y2) +
 		3 * x3 * (-y1 + y2) +
 		x2 * (2 * y1 - 3 * y3 + y4) -
 		x1 * (2 * y2 - 3 * y3 + y4);
-	var r = x3 * (y1 - y2) + x1 * (y2 - y3) + x2 * (-y1 + y3);
+	let r = x3 * (y1 - y2) + x1 * (y2 - y3) + x2 * (-y1 + y3);
 
 	return quadSolve(p, q, r)
 		.filter(function(t) {
@@ -374,6 +377,9 @@ function solveInflections(x1, y1, x2, y2, x3, y3, x4, y4) {
 		})
 		.sort(byNumber);
 }
+
+const SEGMENT_COUNT_MIN = 1;
+const SEGMENT_COUNT_MAX = 8;
 
 /*
  * Approximate cubic Bezier curve defined with base points p1, p2 and control points c1, c2 with
@@ -383,22 +389,34 @@ function solveInflections(x1, y1, x2, y2, x3, y3, x4, y4) {
  * In general the method is the same as described here: https://fontforge.github.io/bezier.html.
  */
 function _cubicToQuad(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y, errorBound, check) {
-	var p1 = new Point(p1x, p1y);
-	var c1 = new Point(c1x, c1y);
-	var c2 = new Point(c2x, c2y);
-	var p2 = new Point(p2x, p2y);
-	var pc = calcPowerCoefficients(p1, c1, c2, p2);
-	var a = pc[0],
+	let p1 = new Point(p1x, p1y);
+	let c1 = new Point(c1x, c1y);
+	let c2 = new Point(c2x, c2y);
+	let p2 = new Point(p2x, p2y);
+	let pc = calcPowerCoefficients(p1, c1, c2, p2);
+	let a = pc[0],
 		b = pc[1],
 		c = pc[2],
 		d = pc[3];
 
-	var approximation;
-	for (var segmentsCount = 1; segmentsCount <= 8; segmentsCount++) {
+	let approximation;
+	for (
+		let segmentsCount = SEGMENT_COUNT_MIN;
+		segmentsCount <= SEGMENT_COUNT_MAX;
+		segmentsCount++
+	) {
 		approximation = [];
 		let bad = false;
-		for (var t = 0; t < 1; t += 1 / segmentsCount) {
-			const seg = processSegment(a, b, c, d, t, t + 1 / segmentsCount, check);
+		for (let t = 0; t < segmentsCount; t += 1) {
+			const seg = processSegment(
+				a,
+				b,
+				c,
+				d,
+				t / segmentsCount,
+				(t + 1) / segmentsCount,
+				check
+			);
 			if (seg || !check) {
 				approximation.push(seg);
 			} else {
@@ -434,18 +452,18 @@ function cubicToQuad(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y, errorBound) {
 	const appro0 = _cubicToQuad(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y, errorBound, true);
 	if (appro0) return appro0;
 
-	var inflections = solveInflections(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y);
+	let inflections = solveInflections(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y);
 
 	if (!inflections.length) {
 		return _cubicToQuad(p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y, errorBound);
 	}
 
-	var result = [];
-	var curve = [p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y];
-	var prevPoint = 0;
-	var quad, split;
+	let result = [];
+	let curve = [p1x, p1y, c1x, c1y, c2x, c2y, p2x, p2y];
+	let prevPoint = 0;
+	let quad, split;
 
-	for (var inflectionIdx = 0; inflectionIdx < inflections.length; inflectionIdx++) {
+	for (let inflectionIdx = 0; inflectionIdx < inflections.length; inflectionIdx++) {
 		split = subdivideCubic(
 			curve[0],
 			curve[1],
